dda8964eb5bd896211197851cbccd44bb1710cd6
Nhan <ducnhan23898@gmail.com>
2021-03-05 16:59:57 +0700

    Updates

---
+++ precache-manifest.f6d62f32cab01163a4d814cb2f9a4a9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f30d6610e1c0f88f5641c94dae33243",
+    "url": "/todo-ReactJS/index.html"
+  },
+  {
+    "revision": "ea80ff166faee83a147a",
+    "url": "/todo-ReactJS/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a543b8a02eabec3282af",
+    "url": "/todo-ReactJS/static/js/2.7c42ecad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-ReactJS/static/js/2.7c42ecad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea80ff166faee83a147a",
+    "url": "/todo-ReactJS/static/js/main.35c6684f.chunk.js"
+  },
+  {
+    "revision": "f75bafa3d204a5f15013",
+    "url": "/todo-ReactJS/static/js/runtime-main.7ee585ca.js"
+  }
+]);
